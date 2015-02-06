569d9234e639738f329f62be67d80f3ef49fb774
Ramtin <ramtinnikbakht@yahoo.com>
2015-02-06 02:28:05 -0700

    Started the Status Page

---
+++ crm_faf/StatusPage.java
@@ -0,0 +1,36 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		
+		primaryStage.setTitle("Status Page");
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		pane.setHgap(10);
+		pane.setVgap(10);
+		pane.setPadding(new Insets(25, 25, 25, 25));
+		Scene scene = new Scene(pane, 600, 400);
+		
+		Text sceneTitle = new Text("Current Status");
+		
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
