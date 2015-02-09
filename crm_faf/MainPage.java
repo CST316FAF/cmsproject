6ef8921d64d9f922b4028e5bd83990c45429d146
kgodiwal <kunaalgodiwala@yahoo.com>
2015-02-09 12:06:38 -0700

    Trying to Fix Buttons
    After reading feedback about how the button on the main page that lead
    to the other pages would work, I have attempted to try another way.
    However, I still haven’t tested it yet.

--- crm_faf/MainPage.java
+++ crm_faf/MainPage.java
@@ -10,23 +10,21 @@ import javafx.stage.Stage;
  
 public class MainPage extends Application {
 
+	//still havent tested this yet
+    Button btn = new Button("Customer Window UI"); //creates a button named btn with the label Customer Window UI
+	HBox hbBtn = new HBox(20); //creates a layout pane named hbBtn with a spacing of 20 pixels
+	hbBtn.setAlignment(Pos.TOP_RIGHT); //positions the button in the top right of the page
+	
+	btn.setOnAction(new EventHandler<ActionEvent>() { //registers an event handler that sets the actiontarget object below to Customer Window UI button pressed when the user presses the button
+	
+ 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Main Page");
-        Button btn1 = new Button();
-        btn1.setText("New Customer Page");
-        btn1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Click");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn1);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    public void handle(ActionEvent e) {
+        actiontarget.setText("Customer Window UI button pressed");
     }
+	});
+	
+	
     // no clue what you were trying to do here but all the methods have the same 
     // name and adksfj;asdlkfj;lasdfj;asklj needs refactoring
     // copy pasting repeatedly is a no no... especially if it doesn't work
