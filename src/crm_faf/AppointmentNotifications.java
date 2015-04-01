4fcf37a4cad30cf6be6326bc9dd2a9f3332ee7aa
kgodiwal <kunaalgodiwala@yahoo.com>
2015-04-01 12:13:09 -0700

    Changes with Testing, etc.
    Attempting to perform code coverage and static analysis testing

--- src/crm_faf/AppointmentNotifications.java
+++ src/crm_faf/AppointmentNotifications.java
@@ -2,10 +2,15 @@
 package crm_faf;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
