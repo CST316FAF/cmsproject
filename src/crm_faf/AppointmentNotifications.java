a3f72e7e374725b859dfe3b1c94577b3ed52bd1d
kgodiwal <kunaalgodiwala@yahoo.com>
2015-04-05 10:48:52 -0700

    Working Version of AppointmentNotifications.java
    This version works and now I am going to add a feature on the main page
    of the project that will reveal a message when an email is sent. Also,
    I will perform quality testing.

--- src/crm_faf/AppointmentNotifications.java
+++ src/crm_faf/AppointmentNotifications.java
@@ -1,4 +1,3 @@
-/*
 package crm_faf;
 
 import java.util.Properties;
@@ -9,81 +8,42 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.swing.JOptionPane;
 
 public class AppointmentNotifications {
 
    public static void main(String[] args) {
-       
-      //Recipient's email ID needs to be mentioned.
-      String to = “kunaalgodiwala@gmail.com”; //change accordingly
-
-      //Sender's email ID needs to be mentioned
-      String from = “kunaalgodiwala@gmail.com”; //change accordingly
-      final String username = “kunaalgodiwala”; //change accordingly
-      final String password = “Godiwala123”; //change accordingly
 
       String host = "smtp.gmail.com";
 
       Properties props = new Properties();
+      props.put("mail.smtp.host", "smtp.gmail.com");
+      props.put("mail.smtp.socketFactory.port", "465");
+      props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
       props.put("mail.smtp.auth", "true");
-      props.put("mail.smtp.starttls.enable", "true");
-      props.put("mail.smtp.host", host);
-      props.put("mail.smtp.port", "Enter Port Number Here");
+      props.put("mail.smtp.port", "465");
 
-      Session session;
-       session = Session.getInstance(props,
-               new javax.mail.Authenticator() {
-                   @Override
-                   protected PasswordAuthentication getPasswordAuthentication() {
-                       return new PasswordAuthentication(username, password);
-                   }
-               });
+      Session session = Session.getDefaultInstance(props,
+              new javax.mail.Authenticator() {
+              protected PasswordAuthentication getPasswordAuthentication() {
+              return new PasswordAuthentication("abc@gmail.com", "password");
+              }
+              }
+                      
+   );
+        try{
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("abc@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("abc@gmail.com"));
+            message.setSubject("Reminder");
+            message.setText("Upcoming Appointment");
+            Transport.send(message);
+            JOptionPane.showMessageDialog(null, "Email Sent!");
 
-      try {
-         //Create a default MimeMessage object
-         Message message = new MimeMessage(session);
-
-         //Set From
-         message.setFrom(new InternetAddress(from));
-
-         //Set To
-         message.setRecipients(Message.RecipientType.TO,
-         InternetAddress.parse(to));
-
-         //Set Subject
-         message.setSubject(“Reminder of Upcoming Appointment”);
-          String date = null;
-          String time = null;
-
-         //Set the actual message
-         message.setText("Hello, this is a friendly reminder that you have on upcoming appointment on" + date + time);
-
-         //Send message
-         Transport.send(message);
-
-         //System.out.println(“Email Sent!“);
-
-      } catch (MessagingException e) {
-            throw new RuntimeException(e);
-      }
-   }
-
-    private static class Upcoming {
-
-        public Upcoming() {
+        } catch(Exception e){
+        
+        JOptionPane.showMessageDialog(null, e);
         }
-    }
 
-    private static class Reminder {
-
-        public Reminder() {
-        }
-    }
-
-    private static class Email {
-
-        public Email() {
-        }
-    }
+      
 }
-*/
