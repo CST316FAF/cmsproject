276236c30a76f44ce847d0baee0fe5d06768fad4
kgodiwal <kunaalgodiwala@yahoo.com>
2015-04-08 18:03:12 -0700

    Test

--- test/crm_faf/AppointmentNotificationsTest.java
+++ test/crm_faf/AppointmentNotificationsTest.java
@@ -5,7 +5,6 @@
  */
 package crm_faf;
 
-import javafx.stage.Stage;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -39,53 +38,14 @@ public class AppointmentNotificationsTest {
     }
 
     /**
-     * Test of newMessage method, of class AppointmentNotifications.
+     * Test of emailNotify method, of class AppointmentNotifications.
+     * 
+     *  Blackbox test
      */
     @Test
-    public void testNewMessage() {
-        System.out.println("newMessage");
-        AppointmentNotifications instance = new AppointmentNotifications();
-        instance.newMessage();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of start method, of class AppointmentNotifications.
-     * @throws java.lang.Exception
-     */
-    @Test
-    public void testStart() throws Exception {
-        System.out.println("start");
-        Stage primaryStage = null;
-        AppointmentNotifications instance = new AppointmentNotifications();
-        instance.start(primaryStage);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    private static class AppointmentNotifications {
-
-        public AppointmentNotifications() {
-        }
-
-        private void start(Stage primaryStage) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        private void newMessage() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-
-    /**
-     * Test of main method, of class AppointmentNotifications.
-     */
-    @Test
-    public void testMain() {
+    public void testEmailNotify() {
         System.out.println("emailNotify");
-        //String[] args = null;
-        crm_faf.AppointmentNotifications.emailNotify();
+        AppointmentNotifications.emailNotify();
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
