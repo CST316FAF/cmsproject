53446cd4440a8642dbc89fc4986cb9eff35fe1a2
austinpruitt <austinpruitt21@gmail.com>
2015-04-13 11:49:39 -0700

    Added more test info to LoginScreenTest, tried to get CreateUserTest not to fail.

--- test/crm_faf/LoginScreenTest.java
+++ test/crm_faf/LoginScreenTest.java
@@ -58,12 +58,56 @@ public class LoginScreenTest {
         box.setText("test");
         PasswordField pbox = new PasswordField();
         pbox.setText("testtest");
-        instance.setPwBox(new PasswordField());
+        //instance.setPwBox(pbox);
         instance.setUserTextField(box);
         instance.setPwBox(pbox);
         instance.getuserTextField().setText("test");
         assert(instance.Buttons());
         
+        TextField box1 = new TextField();
+        box1.setText("test");
+        PasswordField pbox1 = new PasswordField();
+        pbox1.setText("test1");
+        instance.setPwBox(pbox1);
+        instance.setUserTextField(box1);
+        instance.getuserTextField().setText("test");
+        assert(instance.Buttons());
+        
+        TextField box2 = new TextField();
+        box2.setText("test");
+        PasswordField pbox2 = new PasswordField();
+        pbox2.setText("");
+        instance.setPwBox(pbox2);
+        instance.setUserTextField(box2);
+        instance.getuserTextField().setText("test");
+        assert(instance.Buttons());
+        
+        TextField box3 = new TextField();
+        box3.setText("");
+        PasswordField pbox3 = new PasswordField();
+        pbox3.setText("");
+        instance.setPwBox(pbox3);
+        instance.setUserTextField(box3);
+        instance.getuserTextField().setText("test");
+        assert(instance.Buttons());
+        
+        TextField box4 = new TextField();
+        box4.setText("");
+        PasswordField pbox4 = new PasswordField();
+        pbox4.setText("testtest");
+        instance.setPwBox(pbox4);
+        instance.setUserTextField(box4);
+        instance.getuserTextField().setText("test");
+        assert(instance.Buttons());
+        
+        TextField box5 = new TextField();
+        box5.setText("");
+        PasswordField pbox5 = new PasswordField();
+        pbox5.setText("test1");
+        instance.setPwBox(pbox5);
+        instance.setUserTextField(box5);
+        instance.getuserTextField().setText("test");
+        assert(instance.Buttons());
     
     }
     
